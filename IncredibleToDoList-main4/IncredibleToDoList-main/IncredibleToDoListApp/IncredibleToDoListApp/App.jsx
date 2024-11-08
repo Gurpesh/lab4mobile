cb975b86467026c26f86b4c4506e66ab3e9af47c
Gurpesh Singh <165330149+Gurpesh@users.noreply.github.com>
2024-11-08 10:57:00 -0700

    Add files via upload

---
+++ IncredibleToDoList-main4/IncredibleToDoList-main/IncredibleToDoListApp/IncredibleToDoListApp/App.jsx
@@ -0,0 +1,36 @@
+/**
+ * My To Do List App
+ *
+ * @format
+ */
+
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import ToDoForm from './components/ToDoForm';
+import ToDoList from './components/ToDoList';
+
+function App() {
+  const [tasks, setTasks] = useState(['Do laundry', 'Go to gym', 'Walk dog']);
+
+  // Function to add a new task
+  const addTask = (task) => {
+    setTasks((prevTasks) => [...prevTasks, task]);
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {/* Passing addTask function as a prop */}
+      <ToDoForm addTask={addTask} />
+      <ToDoList tasks={tasks} />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
+
+export default App;
